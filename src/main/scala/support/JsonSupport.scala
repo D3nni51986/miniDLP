0c0c4555697d1b8eaeb017d7e2ac7d8293b03771
d3nni51986 <d3nni5.86@gmail.com>
2018-05-06 23:28:57 +0300

    .

---
+++ src/main/scala/support/JsonSupport.scala
@@ -0,0 +1,11 @@
+package support
+
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import models.{ScanFileRequest, ScanServiceInput, ScanServiceResult}
+import spray.json.DefaultJsonProtocol
+
+trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
+  implicit val scanServiceInputFormat = jsonFormat2(ScanServiceInput)
+  implicit val scanServiceResultFormat = jsonFormat2(ScanServiceResult)
+  implicit val scanFileFormat = jsonFormat2(ScanFileRequest)
+}
